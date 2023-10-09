6c92f788bb7a1d2e647014ab88a0b4f8d4906c3c
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-10-10 01:12:14 +0300

    add component Main

---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,10 @@
+import styles from './main.module.css';
+import { API_KEY, WEATHER_API_URL } from '../../components/Api';
+
+export default function Main() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+};
