bd12378da664ef4558ce8a1a7f41175e6b97de0f
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-10-10 01:21:06 +0300

    fix errors in components imports

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from "../Api";
+import { GEO_API_URL, geoApiOptions } from "../API";
 import styles from "./search.module.css";
 
 export default function Search({ onSearchChange }) {
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,5 +1,5 @@
 import styles from './main.module.css';
-import { API_KEY, WEATHER_API_URL } from '../../components/Api';
+import { API_KEY, WEATHER_API_URL } from '../../components/API';
 
 export default function Main() {
   return (
