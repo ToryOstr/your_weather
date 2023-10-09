1bcafb75944a3534ab6ddfe2b95aa42eec152979
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-10-10 00:54:43 +0300

    add component App

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import WeatherApp from "../WeatherApp/WeatherApp";
+import styles from "./app.module.css";
+
+export default function App() {
+  return <WeatherApp />;
+}
